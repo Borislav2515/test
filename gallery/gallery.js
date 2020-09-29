cb08cbb079f3614eb08057de44862c24efa84b9b
Borislav2515 <61460569+Borislav2515@users.noreply.github.com>
2020-09-29 09:20:05 +0300

    Delete gallery.js

--- gallery/gallery.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-let menuMobile = document.getElementById("mobile-menu");
-function scrollFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 80) {
-        menuMobile.style.height = "30px";
-    } else {
-        menuMobile.style.height = "200px";
-    }
-}
